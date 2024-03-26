e2df23079abe2d93b1a8ad24972f3f478c53e66b
kareng13 <13karenglez@gmail.com>
2024-03-26 15:55:11 -0500

    Revert "cambios detalles"
    This reverts commit f5a6af11080b7a8eec81b24ab28f8fdc971e4afe.

--- src/App.jsx
+++ src/App.jsx
@@ -54,81 +54,67 @@ function App() {
   return (
     <div className="section_speech">
       <div className="container_speech">
-        <div className="row">
-            <div className="col-md-6">
-              <img src="https://i.pinimg.com/736x/f0/7f/c5/f07fc577527b4c11e1b489be8c2dc29a.jpg" alt="" />
-            </div>
-            </div>
-            <div className="col-md-6">
         <div className="content_speech">
-          <div className="row">
-            <div className="col">
-              <h3 className="mb-4">Speech to text</h3>
-              {transcript ? (
-                <h6>{transcript}</h6>
-              ) : (
-                <h6 className="mb-4">Press the microphone button and start speaking</h6>
-              )}
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div className="icon_speech">
-                <div className="icon_speech_content">
-                  <button
-                    className="btn_icons"
-                    id="icon_item1"
-                    onClick={toggleListening}
+          <h3 className="mb-4">Speech to text</h3>
+          {transcript ? (
+            <h6>{transcript}</h6>
+          ) : (
+            <h6 className="mb-4">Press the microphone button and start speaking</h6>
+          )}
+
+          <div className="icon_speech">
+            <div className="icon_speech_content">
+              <button
+                className="btn_icons"
+                id="icon_item1"
+                onClick={toggleListening}
+              >
+                {isListening ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-mic-mute"
+                    viewBox="0 0 16 16"
                   >
-                    {isListening ? (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-mic-mute"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4 4 0 0 0 12 8V7a.5.5 0 0 1 1 0zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a5 5 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4m3-9v4.879l-1-1V3a2 2 0 0 0-3.997-.118l-.845-.845A3.001 3.001 0 0 1 11 3" />
-                        <path d="m9.486 10.607-.748-.748A2 2 0 0 1 6 8v-.878l-1-1V8a3 3 0 0 0 4.486 2.607m-7.84-9.253 12 12 .708-.708-12-12z" />
-                      </svg>
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-mic"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5" />
-                        <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3" />
-                      </svg>
-                    )}
-                  </button>
-                  <button className="btn_icons" id="icon_item2" onClick={clearTranscript}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-counterclockwise"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"
-                      />
-                      <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466" />
-                    </svg>
-                  </button>
-                </div>
-                </div>
-                </div>
+                    <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4 4 0 0 0 12 8V7a.5.5 0 0 1 1 0zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a5 5 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4m3-9v4.879l-1-1V3a2 2 0 0 0-3.997-.118l-.845-.845A3.001 3.001 0 0 1 11 3" />
+                    <path d="m9.486 10.607-.748-.748A2 2 0 0 1 6 8v-.878l-1-1V8a3 3 0 0 0 4.486 2.607m-7.84-9.253 12 12 .708-.708-12-12z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-mic"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5" />
+                    <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3" />
+                  </svg>
+                )}
+              </button>
+              <button className="btn_icons" id="icon_item2" onClick={clearTranscript}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-counterclockwise"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2z"
+                  />
+                  <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
